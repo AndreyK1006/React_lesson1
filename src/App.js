b0ca75bda5d8e30e5b97b248e53f43f806e08df7
AndreyK <kavaboonga007@gmail.com>
2022-03-14 10:40:32 +0500

    master

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Message from "./Message";
+function App() {
+  return (
+    <div className="App">
+      let name = "Текст для Message";
+      <Message />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Message.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Message() {
+    return (
+        <div className="App">
+            Первый проект
+        </div>
+    );
+}
+
+export default Message;
